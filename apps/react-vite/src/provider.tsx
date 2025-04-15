282dec2ef28aa48c95d2c554608f7d3afa6fe1f1
I Putu Saputrayana <ikhyan7@gmail.com>
2025-04-15 16:30:44 +0800

    bump

--- apps/react-vite/src/provider.tsx
+++ apps/react-vite/src/provider.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Config, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { XellarKitProvider, defaultConfig, lightTheme, darkTheme } from "@xellar/kit";
+import { XellarKitProvider, defaultConfig, darkTheme } from "@xellar/kit";
 import { polygonAmoy } from "viem/chains";
 
 const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
@@ -20,17 +20,7 @@ export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <XellarKitProvider
-          theme={darkTheme}
-          googleClientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-          telegramConfig={{
-            botId: import.meta.env.VITE_TELEGRAM_BOT_ID,
-            botUsername: import.meta.env.VITE_TELEGRAM_BOT_USERNAME,
-          }}
-          enableWhatsappLogin
-        >
-          {children}
-        </XellarKitProvider>
+        <XellarKitProvider theme={darkTheme}>{children}</XellarKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
