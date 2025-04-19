b730d5d2609dadc39fd951fd7072ff5f15c44bd1
I Putu Saputrayana <ikhyan7@gmail.com>
2025-04-19 14:16:40 +0800

    fix: fix description, fix icon, expose some hooks (#52)

--- packages/xellar-kit/src/index.ts
+++ packages/xellar-kit/src/index.ts
@@ -3,5 +3,7 @@ export { ConnectDialogStandAlone } from './components/dialog/content/connect-dia
 export { defaultConfig } from './config/default-config';
 export { useConnectModal } from './hooks/use-connect-modal';
 export { useXellarAccount } from './hooks/use-xellar-account';
+export { useChainModal } from './hooks/useChainModal';
+export { useProfileModal } from './hooks/useProfileModal';
 export { XellarKitProvider } from './providers/xellar-kit';
 export { darkTheme, lightTheme, type Theme } from './styles/theme';
