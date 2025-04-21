57ee52eca29a4af7473bc750fef03486aba0f775
I Putu Saputrayana <ikhyan7@gmail.com>
2025-04-21 23:04:09 +0800

    Dev (#54)
    * fix: fix description, fix icon, expose some hooks
    
    * fix useXellarAccount

--- packages/xellar-kit/src/hooks/use-xellar-account.ts
+++ packages/xellar-kit/src/hooks/use-xellar-account.ts
@@ -1,41 +1,6 @@
-import { useShallow } from 'zustand/react/shallow';
+import { useXellarAccountStore, XellarAccount } from '@/xellar-connector/store';
 
-import { useBoundStore } from '@/xellar-connector/store';
-
-function decodeJWT(token: string) {
-  const base64Url = token?.split('.')[1] ?? '';
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join('')
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
-interface XellarAccount {
-  email: string;
-  provider: string;
-  uid: string;
-  address: {
-    network: string;
-    address: string;
-  }[];
-  mode: string;
-}
-
-export const useXellarAccount = () => {
-  const token = useBoundStore(useShallow(state => state.token));
-
-  if (!token) {
-    return null;
-  }
-
-  const decodedToken = decodeJWT(token);
-
-  return decodedToken as XellarAccount;
+export const useXellarAccount = (): XellarAccount | null => {
+  const account = useXellarAccountStore(state => state.account);
+  return account;
 };
--- packages/xellar-kit/src/utils/string.ts
+++ packages/xellar-kit/src/utils/string.ts
@@ -1,3 +1,18 @@
 export const truncateAddress = (address: string) => {
   return `${address.slice(0, 6)}····${address.slice(-4)}`;
 };
+
+export function decodeJWT(token: string) {
+  const base64Url = token?.split('.')[1] ?? '';
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
